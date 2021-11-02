d1fc3184f990a01db9535a5cab561522be85d7a6
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2021-11-02 23:36:21 +0900

    app screen styling

--- App.tsx
+++ App.tsx
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+export default function App () {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appBar}>
+        <View style={styles.appBarInner}>
+          <Text style={styles.appBarTitle}>シッタゲキレイ</Text>
+        </View>
+      </View>
+
+      <View>
+        <View style={styles.announceContent}>
+          <Text style={styles.announceText}>いいから{'\n'}コードを書け！！</Text>
+        </View>
+        <View style={styles.onOffContent}>
+          <Text style={styles.onOffBtn}>ボタン</Text>
+          <Text style={styles.onOffText}>無効</Text>
+        </View>
+        <View style={styles.noteContent}>
+          <Text style={styles.noteText}>朝・昼・晩{'\n'}お知らせ！！</Text>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFFFF'
   },
-});
+  appBar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#FFCF87',
+    justifyContent: 'flex-end'
+  },
+  appBarInner: {
+    alignItems: 'center'
+  },
+  appBarTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#FFFFFF',
+    fontWeight: 'bold'
+  },
+  announceContent: {
+    marginTop: 42,
+    alignItems: 'center'
+  },
+  announceText: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    lineHeight: 48,
+    textAlign: 'center',
+    color: '#555555'
+  },
+  onOffContent: {
+    marginTop: 48,
+    alignItems: 'center'
+  },
+  onOffText: {
+    marginTop: 16,
+    fontSize: 24,
+    lineHeight: 36,
+    color: '#555555'
+  },
+  noteContent: {
+    marginTop: 32,
+    alignItems: 'center'
+  },
+  noteText: {
+    textAlign: 'center',
+    fontSize: 24,
+    lineHeight: 36,
+    fontWeight: 'bold',
+    color: '#555555'
+  }
+})
