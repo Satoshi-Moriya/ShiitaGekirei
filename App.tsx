a8bb0112f2a2473e44962f829790f7f864e34379
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2021-11-05 23:44:46 +0900

    firebaseを一旦無くした

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
-import { initializeApp, getApps } from 'firebase/app'
 
 import MainScreen from './src/screens/MainScreen'
-import { firebaseConfig } from './env'
-
-if (getApps().length < 1) {
-  initializeApp(firebaseConfig)
-}
 
 export default function App () {
   return (
